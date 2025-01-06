be0fb915238e4ad15fc55e2f11e99c6276a4e86e
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-06 11:28:03 +0200

    jaunais gads

--- index.php
+++ index.php
@@ -6,7 +6,17 @@ $config = require ("config.php");
 
 $db = new Database($config["database"]);
 
-$posts = $db->query(); 
+if(isset($_GET["search_query"])&& $_GET["search_query"]!=""){
+    $posts =$db->query("SELECT * FROM posts WHERE content LIKE `%". $_GET["search_query"]  ."%`")-> fetchALL();;
+}
+
+echo "<h1>Blogs</h1>";
+echo "<form>";
+echo "<input name=`search_query` />";
+echo "<button>Meklet</button>";
+
+echo "</form>";
+
 $posts=$db->query("SELECT * FROM posts")-> fetchALL();
 
 echo "<ul>";
@@ -18,5 +28,5 @@ foreach($posts as $🤣){
 }
 echo "</ul>";
 
-
+ 
 
